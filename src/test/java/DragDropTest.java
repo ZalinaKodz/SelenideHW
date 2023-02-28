55a229873e1c7778932a9034119269e6895f3f03
Zalina Kodz <kod3ik87@gmail.com>
2023-03-01 00:18:08 +0300

    added hw DragAndDrop

---
+++ src/test/java/DragDropTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDropTest {
+    @Test
+    void changeElements() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
